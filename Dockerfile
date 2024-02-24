26c7e4bce4c9ec010febb796227695534dbefb74
Michael Squance <mike@squancey.co.uk>
2024-02-24 21:36:49 +0000

    setting up app

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ruby:3.3.0-alpine
+RUN apk update
+ENV PIP_BREAK_SYSTEM_PACKAGES 1
+RUN apk --no-cache add curl build-base postgresql-dev tzdata nodejs chromium chromium-chromedriver python3 python3-dev py3-pip yarn procps bash bash-completion sudo openssh docker git less graphviz libxslt-dev libxml2-dev 
+RUN pip install --upgrade pip
+RUN pip3 install -U selenium
+RUN curl https://cli-assets.heroku.com/install.sh | sh
+WORKDIR /app
+RUN bundle config set force_ruby_platform true
+RUN bundle config build.nokogiri --use-system-libraries
+COPY Gemfile Gemfile.lock /app/
---
+++ scripts/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+HOST=0.0.0.0
+PORT=3000
+DEBUG_PORT=1234
+DISPATCHER_PORT=26162
+
+PID="tmp/pids/server.pid"
+if [ -f $PID ]; then
+  rm $PID
+fi
+
+echo "-Installing the bundle (this may take a little while if the volume is empty):"
+bundle config set force_ruby_platform true
+bundle install
+
+echo "- Configuring databases:"
+bundle exec rake db:prepare
+
+echo "- Installing node packages with yarn:"
+yarn install
+
+echo "- Starting rails (with debugging enabled):"
+#(trap 'kill 0' SIGINT;
+#  rdebug-ide --skip_wait_for_start -h $HOST -p $DEBUG_PORT --dispatcher-port $DISPATCHER_PORT -- ./bin/rails s -b $HOST -p $PORT &
+#  bundle exec sidekiq > ./log/sidekiq.log &
+#  ./scripts/webpack-docker-entrypoint.sh > ./log/webpack.log
+#)
+./bin/rails s -b $HOST -p $PORT
