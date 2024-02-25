f49cd621a1af07e6eb64c07837e0313adedeb7b4
Michael Squance <mike@squancey.co.uk>
2024-02-25 16:07:15 +0000

    Setting up uuid for id in database

--- db/migrate/20240225153649_create_game_systems.rb
+++ db/migrate/20240225153649_create_game_systems.rb
@@ -1,6 +1,6 @@
 class CreateGameSystems < ActiveRecord::Migration[7.1]
   def change
-    create_table :game_systems do |t|
+    create_table :game_systems, id: :uuid do |t|
       t.string :name
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_215421) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_25_153649) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "game_systems", force: :cascade do |t|
+  create_table "game_systems", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
