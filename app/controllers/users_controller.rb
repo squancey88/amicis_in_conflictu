74949a7639179be6948e7f78936f0a9848a78dbc
Michael Squance <mike@squancey.co.uk>
2024-05-26 20:00:55 +0000

    Sorting out user management and a few responsive updates

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :set_user
+  before_action :check_access
+
+  def show
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :show, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def check_access
+    redirect_to(root_url) unless current_user == @user
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:firstname, :surname, :nickname, :email)
+  end
+end
