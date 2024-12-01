0c2d51c0725c43045a237a683f9708557a172a0c
Michael Squance <mike@squancey.co.uk>
2024-12-01 22:52:45 +0000

    Fixing bug for nil bale_value

--- app/models/unit_stat.rb
+++ app/models/unit_stat.rb
@@ -6,6 +6,6 @@ class UnitStat < ApplicationRecord
     change = unit.unit_applied_modifiers.includes(unit_stat_modifier: {unit_stat_changes: :unit_stat_definition})
       .where(unit_stat_modifier: {unit_stat_changes: {unit_stat_definition: unit_stat_definition}})
       .sum("unit_stat_changes.stat_change")
-    base_value + change
+    base_value + change if base_value
   end
 end
