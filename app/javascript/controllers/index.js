fd8e24145363dc90b692695241c951542f6fb2d6
Michael Squance <mike@squancey.co.uk>
2025-05-07 14:59:18 +0000

    Fixing broken JS reference

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,7 +4,7 @@ import EditorController from './editorjs_controller';
 import JsonEditorController from './json_editor_controller';
 import NewGameModalController from '../../components/new_game_modal_component/new_game_modal_controller';
 import TurnBasedGameFormController from '../../components/game_forms/turn_based_component/turn_based_controller';
-import ArmySelectorController from '../../components/army_selector_component/army_selector_controller';
+import ArmySelectorController from '../../components/player_forms/wargames/army_selector_component/army_selector_controller';
 import GameFormController from '../../components/game_form_component/game_form_controller';
 import CampaignAndArmySelectorController from '../../components/game_systems/campaign_and_army_selector_component/campaign_and_army_selector_controller';
 import QuestEventsDetailsFormContrller from '../../components/quests/events/details_form_component/details_form_controller';
