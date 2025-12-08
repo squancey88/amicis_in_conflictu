235497d447d67a74997a4c53566ad6ff9fc2163b
Mike Squance <mike@squancey.co.uk>
2025-12-08 12:18:05 +0000

    Adding test to cover external data

--- spec/requests/api/games_spec.rb
+++ spec/requests/api/games_spec.rb
@@ -91,6 +91,35 @@ RSpec.describe "/api/games", type: :request do
           game.reload
           expect(game.game_system).to eq(new_game_system)
         end
+
+        context "with external data" do
+          let(:new_attributes) {
+            {
+              game_system_id: new_game_system.id,
+              data: {
+                external_data: {
+                  start_health: 40,
+                  turn_tracking: [
+                    {end_time: "2025-11-03T19:45:28.473Z", start_time: "2025-11-03T19:45:26.721Z", current_player_index: 0},
+                    {end_time: "2025-11-03T19:46:27.155Z", start_time: "2025-11-03T19:45:28.476Z"},
+                    {end_time: "2025-11-03T19:46:30.867Z", start_time: "2025-11-03T19:46:27.158Z", current_player_index: 0},
+                    {end_time: "2025-11-03T19:46:37.332Z", start_time: "2025-11-03T19:46:30.870Z", current_player_index: 1},
+                    {end_time: "2025-11-03T19:46:56.881Z", start_time: "2025-11-03T19:46:37.335Z", current_player_index: 2}
+                  ]
+                }
+              }
+            }
+          }
+
+          it "with external data" do
+            patch_with_token(user, api_game_url(game), params: {game: new_attributes})
+            game.reload
+            expect(game.game_system).to eq(new_game_system)
+            expect(game.data["external_data"]["start_health"]).to eq("40")
+            expect(game.data["external_data"]["turn_tracking"].length).to eq(5)
+            expect(game.data["external_data"]["turn_tracking"].first["current_player_index"]).to eq("0")
+          end
+        end
       end
     end
   end
