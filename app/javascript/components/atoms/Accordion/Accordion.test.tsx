6e40a44f521310f33cb53d2b3cfe0e2dc057a839
Mike Squance <mike@squancey.co.uk>
2026-05-04 13:18:07 +0100

    Fixing some jest tests

--- app/javascript/components/atoms/Accordion/Accordion.test.tsx
+++ app/javascript/components/atoms/Accordion/Accordion.test.tsx
@@ -1,9 +1,54 @@
-import React from "react"
-import { render, screen } from "@testing-library/react"
-import Accordion from "."
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Accordion } from ".";
+import userEvent from "@testing-library/user-event";
 
 describe("Accordion", () => {
-  it("renders", () => {
-    render(<Accordion />)
-  })
-})
+  it("renders open", () => {
+    render(
+      <Accordion title="Test title" startOpen={true}>
+        <p>Some content</p>
+      </Accordion>,
+    );
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getByText("Test title")).toBeInTheDocument();
+    expect(screen.getByText("Some content").closest("body")).not.toHaveClass(
+      "hidden",
+    );
+  });
+
+  it("renders closed", () => {
+    render(
+      <Accordion title="Test title" startOpen={false}>
+        <p>Some content</p>
+      </Accordion>,
+    );
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getByText("Test title")).toBeInTheDocument();
+    expect(screen.getByText("Some content").closest(".body")).toHaveClass(
+      "hidden",
+    );
+  });
+
+  it("toggles correctly", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <Accordion title="Test title" startOpen={true}>
+        <p>Some content</p>
+      </Accordion>,
+    );
+
+    expect(screen.getByText("Some content").closest("body")).not.toHaveClass(
+      "hidden",
+    );
+
+    await user.click(screen.getByRole("button"));
+
+    expect(screen.getByText("Some content").closest(".body")).toHaveClass(
+      "hidden",
+    );
+  });
+});
