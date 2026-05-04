3f0b728f3947eb0d05c1ba7aef9c7ea53d6ff10e
Mike Squance <mike@squancey.co.uk>
2026-05-04 12:54:44 +0100

    Proper fix

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Rails.autoloaders.main.ignore(Rails.root.join("lib/generators"))
 
 module GamingNightTracker
   class Application < Rails::Application
@@ -15,7 +14,7 @@ module GamingNightTracker
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks generators])
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
