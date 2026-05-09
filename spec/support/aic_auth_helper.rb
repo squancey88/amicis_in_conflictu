bf5fefd039fe71fd2feaf4060a08bdb13fcb7645
Mike Squance <mike@squancey.co.uk>
2026-05-09 17:08:12 +0100

    Trying to stop flaky login test

--- spec/support/aic_auth_helper.rb
+++ spec/support/aic_auth_helper.rb
@@ -35,5 +35,6 @@ module AicAuthHelper
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_on "Log in"
+    expect(page).to have_current_path(root_path)
   end
 end
