d6d5b2813715bdafc6b0db86285bbaf364155b24
Mike Squance <mike@squancey.co.uk>
2026-05-10 14:45:16 +0100

    Fixing testing

--- app/javascript/components/atoms/Pagination/Pagination.test.tsx
+++ app/javascript/components/atoms/Pagination/Pagination.test.tsx
@@ -1,9 +1,20 @@
-import React from "react"
-import { render, screen } from "@testing-library/react"
-import { Pagination } from "."
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Pagination, PaginationValues } from ".";
 
 describe("Pagination", () => {
+  const pagination = {
+    count: 100,
+    limit: 10,
+    page: 1,
+    last: 10,
+  };
+
+  const onChange = jest.fn();
+
   it("renders", () => {
-    render(<Pagination />)
-  })
-})
+    render(<Pagination pagination={pagination} onChange={onChange} />);
+
+    expect(screen.getByText("1")).toBeInTheDocument();
+  });
+});
