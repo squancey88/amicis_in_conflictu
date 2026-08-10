5b22f359c6fd0d0b679ddef1113039731a1cac7b
Mike Squance <mike@squancey.co.uk>
2026-08-10 20:11:16 +0100

    Fix broken controller

--- app/controllers/api/game_systems_controller.rb
+++ app/controllers/api/game_systems_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class GameSystemsController < BaseController
     def index
-      render json: GameSystem.all.map { GameSystemSerializer.new(_1).serialize }
+      render json: GameSystem.all.map { GameSystemSerializer.new(_1).as_json }
     end
   end
 end
